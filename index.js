755bf20e50378808b6a40149c74b13a3287d1468
unknown <Lucas-vicentte@outlook.com>
2024-08-17 18:07:43 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express")
+const cors = require("cors")
+const {default: axios} = require("axios");
+const app = express()
+const port = 3000
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'build')))
+
+
+
+app.use(express.json())
+app.use(cors({ origin: true}));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+
+
+app.post("/authenticate", async (req, res) => {
+    const {username} = req.body
+
+    try {
+        const r = await axios.put(
+            'https://api.chatengine.io/users/',
+          
+          { username: username, secret: username, first_name: username },
+          { headers: { "Private-Key": "7d544457-843e-495d-ad55-cc28b1c25ba5"}}
+          
+        );
+        return res.status(r.status).json(r.data);
+      } catch (e) {
+        return res.status(e.response.status).json(e.response.data);
+      }
+
+   
+    
+})
+
+
+
+
+
+
+app.listen(port, () =>{
+    console.log(`Servidor rodando na porta ${port}`)
+})
